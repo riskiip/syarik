90381c0d5de683fdddc9580d60933824e29581a0
Muhammad Rizki <muhammadrizky@student.ce.undip.ac.id>
2023-02-19 13:11:18 +0700

    Update UI for homepage component

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -7,42 +7,72 @@ import {Router} from '@angular/router';
   styleUrls: [ './header.component.scss' ]
 })
 export class HeaderComponent implements OnInit {
-  tokenExisted = false;
+  isActive1 = false;
+  isActive2 = false;
+  isActive3 = false;
+  burgerMenuClicked = false;
+
   constructor(private router: Router) {
   }
 
   ngOnInit(): void {
-    this.tokenExisted = localStorage.getItem('myToken') !== null;
   }
 
-  toHome() {
-    localStorage.removeItem('loadAll');
-    this.router.navigate([ '' ]);
+  onClickNav(state: string) {
+    switch (state) {
+      case 'daily' :
+        console.log('ini daily blog');
+        this.isActive1 = true;
+        this.isActive2 = false;
+        this.isActive3 = false;
+        break;
+      case 'design':
+        console.log('ini design blog');
+        this.isActive1 = false;
+        this.isActive2 = true;
+        this.isActive3 = false;
+        break;
+      case 'tutorials':
+        console.log('ini tutorials blog');
+        this.isActive1 = false;
+        this.isActive2 = false;
+        this.isActive3 = true;
+        break;
+    }
   }
 
-  toBlog() {
-    this.router.navigate([ 'article' ]);
+  openMobileMenu() {
+    this.burgerMenuClicked = !this.burgerMenuClicked;
   }
 
-  toAbout() {
-    this.router.navigate([ 'about' ]);
-    localStorage.setItem('loadAll', 'test');
-  }
-
-  toLogin() {
-    this.router.navigate([ 'login' ]);
-  }
-
-  toRegister() {
-    this.router.navigate([ 'register' ]);
-  }
-
-  logout() {
-    localStorage.removeItem('myToken');
-    this.router.navigate([ 'login' ]);
-  }
-
-  toProfile() {
-    this.router.navigate([ 'author' ]);
-  }
+  // toHome() {
+  //   localStorage.removeItem('loadAll');
+  //   this.router.navigate([ '' ]);
+  // }
+  //
+  // toBlog() {
+  //   this.router.navigate([ 'article' ]);
+  // }
+  //
+  // toAbout() {
+  //   this.router.navigate([ 'about' ]);
+  //   localStorage.setItem('loadAll', 'test');
+  // }
+  //
+  // toLogin() {
+  //   this.router.navigate([ 'login' ]);
+  // }
+  //
+  // toRegister() {
+  //   this.router.navigate([ 'register' ]);
+  // }
+  //
+  // logout() {
+  //   localStorage.removeItem('myToken');
+  //   this.router.navigate([ 'login' ]);
+  // }
+  //
+  // toProfile() {
+  //   this.router.navigate([ 'author' ]);
+  // }
 }
